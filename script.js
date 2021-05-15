106915c82addd5481f3e798cba9b3277fa212647
dagarnue98 <dagarnue@teleco.upv.es>
2021-05-15 11:27:01 +0200

    Final Project-Final Version-Working

--- script.js
+++ script.js
@@ -8,10 +8,20 @@ function compute()
     var interest= principal*years*rate/100;
     var year = new Date().getFullYear()+parseInt(years);
     //Asociate the result 
-    document.getElementById("result").innerHTML = "If you deposit " + principal + ",<br/>" +
-        "at an interest rate of " + rate + ",<br/>" +
-        "You will receive an amount of " + interest + ",<br/> " +
-        "in the year "+ year ;
+    
+
+    var notComputeifZero = (principal) > 0
+    if (!notComputeifZero){
+        alert("You cant enter negative numbers or 0 , please enter a Positive Amount");
+        document.getElementById("principal").focus();
+    }
+    else {
+        document.getElementById("result").innerHTML = "If you deposit  <mark>" + principal + "</mark>,<br/>" +
+        "at an interest rate of <mark>" + rate + "</mark>.<br/>" +
+        "You will receive an amount of <mark>" + interest + "</mark>,<br/> " +
+        "in the year <mark> "+ year + "</mark>." ;
+    }
+    
     
 }
 //Function to update the interval Rate
